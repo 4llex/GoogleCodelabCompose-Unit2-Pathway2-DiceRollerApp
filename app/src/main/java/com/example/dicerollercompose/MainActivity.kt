9d0b602049cd55d67cfbc748357def10e8e0d722
Alex Rosa <alexr@gec.inatel.br>
2024-01-22 20:24:47 -0300

    Create a vertical layout

--- app/src/main/java/com/example/dicerollercompose/MainActivity.kt
+++ app/src/main/java/com/example/dicerollercompose/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.dicerollercompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.runtime.Composable
@@ -28,7 +29,12 @@ fun DiceWithButtonAndImage(
         .fillMaxSize()
         .wrapContentSize(Alignment.Center)
 ) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
 
+    }
 }
 
 @Preview(
